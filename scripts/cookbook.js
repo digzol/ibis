e45dd0a1e782a5c99ac73d3dea6b975b5d0fed70
digzol <digzol@hotmail.com>
2019-03-19 03:23:08 -0400

    Removed unused sort functions

--- scripts/cookbook.js
+++ scripts/cookbook.js
@@ -280,15 +280,3 @@ function displayResults(start, end) {
         $varEntry.appendTo("#result-recipes");
     });
 }
-
-function sortByName(a, b) {
-    if (a.getName() > b.getName()) return 1;
-    if (a.getName() < b.getName()) return -1;
-    return 0;
-}
-
-function sortByFEP(a, b) {
-    if (a[2] > b[2]) return -1;
-    if (a[2] < b[2]) return 1;
-    return 0;
-}
