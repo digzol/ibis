bcb73c7ccf8fd915a0645945c32d6320f23c1db3
digzol <digzol@hotmail.com>
2019-04-05 22:42:40 -0400

    Implemented Jekyll markup

---
+++ assets/js/forages.js
@@ -0,0 +1,61 @@
+import {getItemsData} from './model/ItemCollection.js';
+
+$(function () {
+    getItemsData().then(data => {
+        const items = data.getEntries();
+        const forages = items.filter(isForageable).sort(sortForageable);
+        const template = $("#template-forage-entry").html();
+
+        forages.forEach(function (forage) {
+            $("#forage-entries").append(template
+                .replace(new RegExp("{name}", 'g'), forage.name)
+                .replace(new RegExp("{id}", 'g'), forage.id)
+                .replace("{detection}", forage.detection)
+                .replace("{detection-min}", Math.round(forage.detection / 2))
+                .replace("{detection-max}", forage.detection * 2)
+            );
+        });
+
+        onDetectionChange();
+    });
+
+    $("#forage-perception")
+        .on("keyup change", onDetectionChange)
+        .val(localStorage.getItem("perception") || 10);
+
+    $("#forage-exploration")
+        .on("keyup change", onDetectionChange)
+        .val(localStorage.getItem("exploration") || 1);
+});
+
+function isForageable(item) {
+    return 'detection' in item;
+}
+
+function sortForageable(a, b) {
+    if (a.detection < b.detection) return -1;
+    if (a.detection > b.detection) return 1;
+    return 0;
+}
+
+function onDetectionChange() {
+    const p_perception = $("#forage-perception").val();
+    const p_exploration = $("#forage-exploration").val();
+    const p_detection = p_perception * p_exploration;
+
+    localStorage.setItem("perception", p_perception);
+    localStorage.setItem("exploration", p_exploration);
+
+    $("#forage-detection").html(p_detection);
+
+    $("#forage-entries .card").each(function() {
+        const detectionReq = $(this).attr("detection");
+        let percentDetected = Math.round(100 * (2 * p_detection - detectionReq) / (3 * detectionReq));
+
+        percentDetected = (percentDetected > 100) ? 100 : percentDetected;
+        percentDetected = (percentDetected < 0) ? 0 : percentDetected;
+
+        $(".progress-bar", this).css("width", percentDetected + "%");
+        $(".forage-percentage", this).html(percentDetected + "%");
+    })
+}
---
+++ assets/js/model/ItemCollection.js
@@ -0,0 +1,42 @@
+import {Item} from './Item.js';
+
+class ItemCollection {
+    constructor() {
+        this.source = '/assets/json/items.json';
+        this._entries = [];
+
+        this.ready = this.init();
+    }
+
+    async init() {
+        const json = $.getJSON(this.source);
+
+        await json.then((data) => {
+            parseData(this, data)
+        });
+
+        return this;
+    }
+
+    getEntries() {
+        return this._entries;
+    }
+}
+
+function parseData(holder, data) {
+    const map = holder.getEntries();
+
+    data['entries'].forEach(function (entry) {
+        const obj = new Item(entry);
+        const id = obj.id;
+
+        map.push(obj);
+        map[id] = obj;
+    });
+}
+
+const instance = new ItemCollection().ready;
+
+export const getItemsData = async function() {
+    return await instance;
+};
