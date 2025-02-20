ab0931da3fa0e99f75aacc8923c863c3c4aa55a1
digzol <digzol@users.noreply.github.com>
2025-02-20 11:48:57 -0500

    Added small icon set & wounds

--- assets/js/model/Item.js
+++ assets/js/model/Item.js
@@ -4,16 +4,5 @@ export class Item {
         this.name = name;
         this.gfx = gfx || id.replace("herbs-", "herbs/");
         this.icons = icons;
-
-        this.iconString = this._getIconString();
-    }
-
-    _getIconString() {
-        if (this.icons === undefined) {
-            this.iconString = `var(--icon-${this.id})`;
-        } else {
-            this.iconString = this.icons.map((icon) => `var(--icon-${icon})`).join(', ');
-        }
-        return this.iconString;
     }
 }
--- assets/js/spritesheet.js
+++ assets/js/spritesheet.js
@@ -1,73 +1,84 @@
 import {Items} from "./data/ItemsStatic.js";
+import {Icons} from "./data/IconsStatic.js";
 
-const SOURCE_URL = "https://www.havenandhearth.com/mt/r/gfx/invobjs/";
+const SOURCE_URL = "https://www.havenandhearth.com/mt/r/";
 const MAX_COLS = 15;
-const ICON_SIZE = 32;
 const ICON_PADDING = 2;
 
+const icons = GetIconsToDraw();
+
 $(function() {
-  const canvas = $("#spritesheet-canvas");
-  const outputDiv = $("#spritesheet-output-css");
-  const ctx = canvas[0].getContext("2d");
+  const $spritesheetData = $("#spritesheet-data");
+  const $spritesheetDataSmall = $("#spritesheet-data-small");
 
-  const drawnItems = [];
-  let outputString = `.game-icon-1x1 { background-position: ${ICON_PADDING * -1}px ${ICON_PADDING * -1}px; }\n`;
+  displaySheet(icons, $spritesheetData, 32, "game-icon-1x1");
+  displaySheet(icons, $spritesheetDataSmall, 16, "small-icon");
+});
 
-  // Check items to draw
+function GetIconsToDraw() {
+  const icons = [];
+  const sets = [Icons.CharSheet, Icons.Wounds];
+
+  icons.push({ id: "missing", path: "gfx/invobjs/missing" });
+
+  sets.forEach(set => {
+    const path = set.path;
+    for (let ind in set.entries) {
+      const id = set.entries[ind];
+      icons.push({id: id, path: `${path}/${id}` });
+    }
+  });
+
   for (let key in Items) {
     const item = Items[key];
-
     if (item.gfx !== undefined) {
-      drawnItems.push(item);
+      icons.push({id: item.id, path: `gfx/invobjs/${item.gfx}` });
     }
   }
 
+  icons.forEach(icon => {
+    icon.img = document.createElement('img');
+    icon.img.src = SOURCE_URL + icon.path;
+    icon.img.addEventListener("error", () =>
+      console.log(`Couldn't load icon from ${icon.img.src}`));
+  });
+
+  return icons;
+}
+
+function displaySheet(icons, $target, iconSize, cssClass) {
+  const canvas = $("canvas", $target);
+  const outputDiv = $("textarea", $target);
+  const ctx = canvas[0].getContext("2d");
+  let outputString = `.${cssClass} { background-position: ${ICON_PADDING * -1}px ${ICON_PADDING * -1}px; }\n`;
+
   let index = 1;
-  let len = drawnItems.length + 1;
-  let cols = Math.min(len, MAX_COLS);
-  let rows = Math.ceil(len / cols);
-  ctx.width = cols * (ICON_SIZE + ICON_PADDING) + ICON_PADDING;
-  ctx.height = rows * (ICON_SIZE + ICON_PADDING) + ICON_PADDING;
+  const len = icons.length + 1;
+  const cols = Math.min(len, MAX_COLS);
+  const rows = Math.ceil(len / cols);
+  ctx.width = cols * (iconSize + ICON_PADDING) + ICON_PADDING;
+  ctx.height = rows * (iconSize + ICON_PADDING) + ICON_PADDING;
   canvas.attr('width', ctx.width);
   canvas.attr('height', ctx.height);
 
-  // add default icon
-  const defaultIcon = document.createElement('img');
-  defaultIcon.src = SOURCE_URL + "missing";
-  defaultIcon.onload = () => drawIcon(defaultIcon, 0);
-  appendString("missing", 0);
+  icons.forEach((icon, index) => {
+    const pos = GetPos(index, iconSize, cols);
 
-  // Load and draw icons
-  drawnItems.forEach((item, index) => {
-    const img = document.createElement('img');
-    img.src = SOURCE_URL + item.gfx;
-    img.onload = function() {
-      drawIcon(img, index + 1);
-    }
-    img.onerror = function() {
-      console.log(`Couldn't load icon from ${img.src}`);
-      drawIcon(defaultIcon, index + 1);
-    }
+    icon.img.addEventListener("load", () =>
+      ctx.drawImage(icon.img, pos.x, pos.y, iconSize, iconSize));
 
-    appendString(item.id, index + 1);
+    icon.img.addEventListener("error", () =>
+      ctx.drawImage(icons[0].img, pos.x, pos.y, iconSize, iconSize));
+
+    outputString += `.${cssClass}.icon-${icon.id} { background-position: ${pos.x * -1}px ${pos.y * -1}px; }\n`;
   });
 
   outputDiv.html(outputString);
+}
 
-  function drawIcon(img, index) {
-    const pos = GetPos(index)
-    ctx.drawImage(img, pos.x, pos.y, ICON_SIZE, ICON_SIZE);
+function GetPos(index, iconSize, cols) {
+  return {
+    x: (index % cols) * (iconSize + ICON_PADDING) + ICON_PADDING,
+    y: Math.floor(index / cols) * (iconSize + ICON_PADDING) + ICON_PADDING
   }
-
-  function appendString(id, index) {
-    const pos = GetPos(index)
-    outputString += `.game-icon-1x1.icon-${id} { background-position: ${pos.x * -1}px ${pos.y * -1}px; }\n`;
-  }
-
-  function GetPos(index) {
-    return {
-      x: (index % cols) * (ICON_SIZE + ICON_PADDING) + ICON_PADDING,
-      y: Math.floor(index / cols) * (ICON_SIZE + ICON_PADDING) + ICON_PADDING
-    }
-  }
-});
+}
